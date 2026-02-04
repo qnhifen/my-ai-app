e58707bd3698a2530d00d3f8ff65bc5f3729455a
qnhifen <105192872+qnhifen@users.noreply.github.com>
2026-02-05 04:35:43 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: '超级大乐透ai预测大师',
+  webDir: 'dist'
+};
+
+export default config;
